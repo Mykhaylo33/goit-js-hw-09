503e6a22e265d9bcaab8a9254fa6d861b0e5cc90
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-04-10 23:35:28 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,49 @@
+const rfs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+  timerId: null,
+  isStartButtonSwitchedOn: false,
+};
 
+// Поведінка
+function onStartButtonHandler() {
+  if (!rfs.isStartButtonSwitchedOn) {
+    rfs.btnStart.disabled = true;
+    rfs.btnStop.disabled = false;
+    rfs.isStartButtonSwitchedOn = true;
+    rfs.timerId = setInterval(myBodyColor, 1000);
+  }
+}
+
+function onStopButtonHandler() {
+  if (rfs.isStartButtonSwitchedOn) {
+    rfs.btnStart.disabled = false;
+    rfs.btnStop.disabled = true;
+    rfs.isStartButtonSwitchedOn = false;
+    clearInterval(rfs.timerId);
+  }
+}
+
+function myBodyColor() {
+  rfs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+// Стилі
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Взаємодія з користувачем
+rfs.btnStop.disabled = true;
+rfs.btnStart.addEventListener('click', onStartButtonHandler);
+rfs.btnStop.addEventListener('click', onStopButtonHandler);
+
+// Обробник відключення інтервалу
+window.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    onStopButtonHandler();
+  } else {
+    onStartButtonHandler();
+  }
+});
