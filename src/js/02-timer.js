bd9365a38a2b0675fc76cad1fa4ed31ab120df55
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-04-11 00:11:27 +0300

    finished task №2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,7 @@
 import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
 
-// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
-
-Notiflix.Notify.init({
-  timeout: 2500,
-  clickToClose: true,
-});
 
 const rfs = {
   printDaysEl: document.querySelector('[data-days]'),
@@ -32,82 +26,66 @@ const options = {
   onClose(selectedDates) {
     rfs.chosenFutureDateTimeValue = selectedDates[0];
     if (isBtnStartValid()) {
-      setTimeout(attentionText, 10);
+      setTimeout(() => Notiflix.Notify.failure('Please choose a date in the future'), 10);
     }
   },
 };
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+const addLeadingZero = (value) => String(value).padStart(2, '0');
+
+const convertMs = (ms) => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-function onBtnStartHandler() {
-  btnStartSwitchOff();
-  startTimer();
-}
-
-function startTimer() {
+const startTimer = () => {
   if (rfs.intervalId) {
     clearInterval(rfs.intervalId);
   }
   rfs.decrementTimeValue = rfs.chosenFutureDateTimeValue - new Date();
-  rfs.intervalId = setInterval(() => {
-    updateTime();
-  }, 1000);
-}
+  rfs.intervalId = setInterval(updateTime, 1000);
+};
 
-function updateTime() {
+const updateTime = () => {
   render(convertMs(rfs.decrementTimeValue));
   rfs.decrementTimeValue -= 1000;
-}
+};
 
-function render({ days = 0, hours = 0, minutes = 0, seconds }) {
+const render = ({ days = 0, hours = 0, minutes = 0, seconds }) => {
   rfs.printDaysEl.textContent = addLeadingZero(days);
   rfs.printHoursEl.textContent = addLeadingZero(hours);
   rfs.printMinutesEl.textContent = addLeadingZero(minutes);
   rfs.printSecondsEl.textContent = addLeadingZero(seconds);
-}
+};
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
-function isBtnStartValid() {
+const isBtnStartValid = () => {
   rfs.currentDateTimeValue = new Date();
   rfs.deltaTimeValue = rfs.chosenFutureDateTimeValue - rfs.currentDateTimeValue;
-  rfs.deltaTimeValue > 0
-    ? (rfs.btnStartEnable = false)
-    : (rfs.btnStartEnable = true);
+  rfs.btnStartEnable = rfs.deltaTimeValue <= 0;
   rfs.btnStartEl.disabled = rfs.btnStartEnable;
   return rfs.btnStartEnable;
-}
+};
 
-function btnStartSwitchOff() {
-  rfs.btnStartEl.disabled = true;
-}
+const btnStartSwitchOff = () => rfs.btnStartEl.disabled = true;
 
-function attentionText() {
-  Notiflix.Notify.failure('Please choose a date in the future');
-}
+const onBtnStartHandler = () => {
+  btnStartSwitchOff();
+  startTimer();
+};
 
-function attentionConsoleText(text) {
-  console.log(text);
-}
+Notiflix.Notify.init({
+  timeout: 2500,
+  clickToClose: true,
+});
 
 rfs.btnStartEl.addEventListener('click', onBtnStartHandler);
 flatpickr(rfs.dateTimePickerEl, options);
